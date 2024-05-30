fdc83c77565b30466f63e3253e06b464c69ffb72
Maksym ZUBRYTSKYI <maksym.zubrytskyi-ext@ledger.fr>
2024-05-30 12:45:45 +0300

    initial changes

--- README.md
+++ README.md
@@ -1,28 +1,32 @@
-# Grass
-An unofficial Docker Image for [getgrass.io](https://app.getgrass.io/register/?referralCode=VqlevN7hfQLGGiQ)
+# NodePay
+An unofficial Docker Image for [app.nodepay.ai](https://app.nodepay.ai/register?ref=3iXL7IuXXwKQVE6)
 Available on [Docker Hub](https://hub.docker.com/r/camislav/grass)
 
 ## What's Grass?
 Grass allows you to earn passive income by sharing your network bandwidth
 
 ## How to get started?
-1. Register a Grass Account if you don't have one already: [getgrass.io](https://app.getgrass.io/register/?referralCode=VqlevN7hfQLGGiQ)
+1. Register a Nodepay Account if you don't have one already: [app.nodepay.ai](https://app.nodepay.ai/register?ref=3iXL7IuXXwKQVE6)
 2. Either build this image from source, or download it from Docker Hub
-3. Set envriomental variables to their respective values: GRASS_USER and GRASS_PASS
+3. Set envriomental variables to their respective values: NODEPAY_USER and NODEPAY_PASS
 4. You're good to go! Once started, the docker exposes your current network status and lifetime earnings on port 80
 
 ### Docker Run Command
 ```
 docker run -d \
-    --name Grass \
+    --name nodepay \
     -p 8080:80 \
-    -e GRASS_USER=myuser@mail.com \
-    -e GRASS_PASS=mypass \
+    -e NODEPAY_USER=myuser@mail.com \
+    -e NODEPAY_PASS=mypass \
     -e ALLOW_DEBUG=False \
     camislav/grass
 ```
 
-Please replace 8080 with the port you want to be able to access the status with, as well as GRASS_USER and GRASS_PASS
+Please replace 8080 with the port you want to be able to access the status with, as well as NODEPAY_USER and NODEPAY_PASS
+
+## Separate thanks
+I would like to mention [kgregor98](https://github.com/kgregor98/grass) and his project Grass for inspiring me to create this.
+
 
 ## License
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
--- src/main.py
+++ src/main.py
@@ -1,76 +1,64 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
-
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
-
 from selenium.common.exceptions import WebDriverException, NoSuchDriverException
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 import requests
 import os
 import re
-import base64
-from flask import Flask
-import hashlib
-import sys
+from flask import Flask, jsonify
 
-extensionId = 'ilehaonighjijnmpnagapkhpcdbhclfg'
-CRX_URL = "https://clients2.google.com/service/update2/crx?response=redirect&prodversion=98.0.4758.102&acceptformat=crx2,crx3&x=id%3D~~~~%26uc&nacl_arch=x86-64"
-USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36"
+# Example usage
+chrome_version = '124.0.6367.78'
+extension_id = 'lgmpfmgeabnnlemejacfljbmonaomfmm'  # Replace with the actual extension ID
+output_file = "extension.crx"
 
 try:
-    USER = os.environ['GRASS_USER']
-    PASSW = os.environ['GRASS_PASS']
-except:
+    USER = os.environ['NODEPAY_USER']
+    PASSW = os.environ['NODEPAY_PASS']
+except KeyError:
     USER = ''
     PASSW = ''
 
 try:
-    ALLOW_DEBUG = os.environ['ALLOW_DEBUG']
-    if ALLOW_DEBUG == 'True':
-        ALLOW_DEBUG = True
-    else:
-        ALLOW_DEBUG = False
-except:
+    ALLOW_DEBUG = os.environ['ALLOW_DEBUG'] == 'True'
+except KeyError:
     ALLOW_DEBUG = False
 
-# are they set?
 if USER == '' or PASSW == '':
-    print('Please set GRASS_USER and GRASS_PASS env variables')
+    print('Please set NODEPAY_USER and NODEPAY_PASS env variables')
     exit()
 
-if ALLOW_DEBUG == True:
+if ALLOW_DEBUG:
     print('Debugging is enabled! This will generate a screenshot and console logs on error!')
 
-
-#https://gist.github.com/ckuethe/fb6d972ecfc590c2f9b8
-def download_extension(extension_id):
-    url = CRX_URL.replace("~~~~", extension_id)
-    headers = {"User-Agent": USER_AGENT}
-    r = requests.get(url, stream=True, headers=headers)
-    with open("grass.crx", "wb") as fd:
-        for chunk in r.iter_content(chunk_size=128):
-            fd.write(chunk)
-    if ALLOW_DEBUG == True:
-        #generate md5 of file
-        md5 = hashlib.md5(open('grass.crx', 'rb').read()).hexdigest()
-        print('Extension MD5: ' + md5)
-
-
+def download_crx(extension_id, output_file, chrome_version):
+    url_template = "https://clients2.google.com/service/update2/crx?response=redirect&prodversion={}&acceptformat=crx2,crx3&x=id%3D{}%26uc"
+    url = url_template.format(chrome_version, extension_id)
+    headers = {
+        'User-Agent': f"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/{chrome_version} Safari/537.36"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=True, stream=True)
+    if response.status_code == 200:
+        with open(output_file, 'wb') as file:
+            file.write(response.content)
+        print(f"Extension {extension_id} has been downloaded as {output_file}.")
+    else:
+        print(f"Failed to download the extension. Status code: {response.status_code}")
 
 def generate_error_report(driver):
-    if ALLOW_DEBUG == False:
+    if not ALLOW_DEBUG:
         return
-    #grab screenshot
     driver.save_screenshot('error.png')
-    #grab console logs
     logs = driver.get_log('browser')
     with open('error.log', 'w') as f:
         for log in logs:
             f.write(str(log))
             f.write('\n')
-
     url = 'https://imagebin.ca/upload.php'
     files = {'file': ('error.png', open('error.png', 'rb'), 'image/png')}
     response = requests.post(url, files=files)
@@ -78,40 +66,41 @@ def generate_error_report(driver):
     print('Error report generated! Provide the above information to the developer for debugging purposes.')
 
 print('Downloading extension...')
-download_extension(extensionId)
+download_crx(extension_id, output_file, chrome_version)
 print('Downloaded! Installing extension and driver manager...')
 
 options = webdriver.ChromeOptions()
-#options.binary_location = '/usr/bin/chromium-browser'
 options.add_argument("--headless=new")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument('--no-sandbox')
-
-options.add_extension('grass.crx')
+options.add_extension(output_file)
 
 print('Installed! Starting...')
 try:
     driver = webdriver.Chrome(options=options)
-except (WebDriverException, NoSuchDriverException) as e:
+except (WebDriverException, NoSuchDriverException):
     print('Could not start with Manager! Trying to default to manual path...')
     try:
         driver_path = "/usr/bin/chromedriver"
         service = ChromeService(executable_path=driver_path)
         driver = webdriver.Chrome(service=service, options=options)
-    except (WebDriverException, NoSuchDriverException) as e:
+    except (WebDriverException, NoSuchDriverException):
         print('Could not start with manual path! Exiting...')
         exit()
 
-#driver.get('chrome-extension://'+extensionId+'/index.html')
+def set_desktop_resolution(driver, width=1920, height=1080):
+    driver.set_window_size(width, height)
+
 print('Started! Logging in...')
-driver.get('https://app.getgrass.io/')
+set_desktop_resolution(driver, 1920, 1080)
+driver.get('https://app.nodepay.ai/')
 
 sleep = 0
 while True:
     try:
-        driver.find_element('xpath', '//*[@name="user"]')
-        driver.find_element('xpath', '//*[@name="password"]')
-        driver.find_element('xpath', '//*[@type="submit"]')
+        driver.find_element(By.XPATH, '//*[@id="basic_user"]')
+        driver.find_element(By.XPATH, '//*[@id="basic_password"]')
+        driver.find_element(By.XPATH, '//*[@type="submit"]')
         break
     except:
         time.sleep(1)
@@ -123,23 +112,18 @@ while True:
             driver.quit()
             exit()
 
-#find name="user"
-user = driver.find_element('xpath', '//*[@name="user"]')
-passw = driver.find_element('xpath', '//*[@name="password"]')
-submit = driver.find_element('xpath', '//*[@type="submit"]')
+user = driver.find_element(By.XPATH, '//*[@id="basic_user"]')
+passw = driver.find_element(By.XPATH, '//*[@id="basic_password"]')
+submit = driver.find_element(By.XPATH, '//*[@type="submit"]')
 
-#get user from env
 user.send_keys(USER)
 passw.send_keys(PASSW)
 submit.click()
 
-#id="chakra-toast-manager-top-right" is the toast
-
-
 sleep = 0
 while True:
     try:
-        e = driver.find_element('xpath', '//*[contains(text(), "Dashboard")]')
+        e = driver.find_element(By.XPATH, '//*[contains(text(), "Dashboard")]')
         break
     except:
         time.sleep(1)
@@ -152,15 +136,19 @@ while True:
             exit()
 
 print('Logged in! Waiting for connection...')
-driver.get('chrome-extension://'+extensionId+'/index.html')
+driver.get('chrome-extension://' + extension_id + '/index.html')
 sleep = 0
 while True:
     try:
-        driver.find_element('xpath', '//*[contains(text(), "Open dashboard")]')
+        # Add an explicit wait here
+        WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located((By.XPATH, '//a[contains(@class, "rounded-lg") and contains(@href, "dashboard")]/span[contains(text(), "Open Dashboard")]'))
+        )
+        print('Found the "Open Dashboard" link!')
         break
-    except:
+    except Exception as e:
         time.sleep(1)
-        print('Loading connection...')
+        print('Loading connection...', e)
         sleep += 1
         if sleep > 30:
             print('Could not load connection! Exiting...')
@@ -169,45 +157,59 @@ while True:
             exit()
 
 print('Connected! Starting API...')
-#flask api
-app = Flask(__name__)
 
-@app.route('/')
-def get():
+def get_data(driver):
     try:
-        network_quality = driver.find_element('xpath', '//*[contains(text(), "Network quality")]').text
-        network_quality = re.findall(r'\d+', network_quality)[0]
-    except:
+        # Wait for the element indicating network quality to be present
+        network_quality_element = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, "//span[contains(text(), 'Network Quality')]"))
+        )
+        network_quality = network_quality_element.text.split(":")[1].strip()
+    except Exception as e:
         network_quality = False
-        print('Could not get network quality!')
+        print(f'Could not get network quality: {e}')
         generate_error_report(driver)
 
     try:
-        token = driver.find_element('xpath', '//*[@alt="token"]')
-        token = token.find_element('xpath', 'following-sibling::div')
-        epoch_earnings = token.text
+        # Wait for the element indicating earnings to be present
+        earnings_element = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, "//div[@class='h-[64px] bg-grey-primary rounded-lg py-3 px-6 flex flex-col items-center justify-between']/div/span[@class='text-16px font-bold mr-1 truncate']"))
+        )
+        epoch_earnings = earnings_element.text
     except Exception as e:
         epoch_earnings = False
-        print('Could not get earnings!')
+        print(f'Could not get earnings: {e}')
         generate_error_report(driver)
-    
+        
     try:
-        #find all chakra-badge
-        badges = driver.find_elements('xpath', '//*[contains(@class, "chakra-badge")]')
-        #find the one with chakra-text that contains either "Connected" or "Disconnected"
+        # Wait for the badge elements indicating connection status to be present
+        badges = WebDriverWait(driver, 10).until(
+            EC.visibility_of_all_elements_located((By.XPATH, "//span[@class='font-bold text-green']"))
+        )
+        connected = badges[0].text if badges else False  # Assuming you want the text of the first matching element
+    except Exception as e:
         connected = False
-        for badge in badges:
-            text = badge.find_element('xpath', 'child::div//p').text
-            if 'Connected' in text:
-                connected = True
-                break
-    except:
-        connected = False
-        print('Could not get connection status!')
+        print(f'Could not get connection status: {e}')
         generate_error_report(driver)
 
     return {'connected': connected, 'network_quality': network_quality, 'epoch_earnings': epoch_earnings}
 
+# Flask API
+app = Flask(__name__)
 
-app.run(host='0.0.0.0',port=80, debug=False)
+@app.route('/')
+def get_endpoint():
+    try:
+        data = get_data(driver)
+        # Convert None values to False for JSON serialization
+        for key in data:
+            if data[key] is None:
+                data[key] = False
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=False)
+
 driver.quit()
